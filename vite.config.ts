6cdcb72f8cc4eb36767cdb051afd823d5ea50083
ze-pena <jose.pena.dev@gmail.com>
2025-05-06 21:59:04 -0300

    config: set up project path shortcut alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
+
+const env = loadEnv('all', './');
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: env.VITE_APP_ENVIRONMENT === 'dev' ? '/' : '/freelando/',
+  resolve: {
+    alias: {
+      '@assets': '/src/assets',
+      '@components': '/src/components',
+      '@configs': '/src/configs',
+      '@hooks': '/src/hooks',
+      '@interfaces': '/src/interfaces',
+      '@layouts': '/src/layouts',
+      '@pages': '/src/pages',
+      '@services': '/src/services',
+      '@store': '/src/store',
+    },
+  },
 })
