06025a195b861dff048a6febdf81dd3460244793
ze-pena <jose.pena.dev@gmail.com>
2025-05-10 00:40:20 -0300

    feat: use "Register Form" component in main app file

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import Default from '@layouts/Default';
+import RegisterForm from '@components/forms/RegisterForm';
 
 export default function App() {
   return (
     <Default>
-      <h3>Hello World!</h3>;
+      <RegisterForm />
     </Default>
   );
 }
